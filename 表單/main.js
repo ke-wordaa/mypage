11e445cd032f5e466d64116d5a225214d77dc412
kevinjj <h849179@gmail.com>
2022-06-21 11:05:08 +0800

    a

--- 表單/main.js
+++ 表單/main.js
@@ -1,23 +1,32 @@
-function a() {
-    let username = Number(document.getElementById("um").value)
-    let password = document.getElementById("pwd").value
-    var no= [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47]
-    for (let i = 0; i <= username; i++) {
-        console.log(i)
-        if (username == no[i]) {
-            if(password == "163")
-            {
-                alert(String(no[i])+'你好')
-            }
-            else
-            {
-                alert("密碼錯誤!")
-                break
+
+$("body").ready(function () {
+    $("#push").click(function () 
+    { 
+        alert("a")
+        var username = $("#um").val();
+        var password = $("#pwd").val();
+        var no= [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47]
+        console.log(password)
+        
+        for (let i = 0; i <= username; i++) {
+            console.log(i)
+            if (username == no[i]) {
+                if(password == "163")
+                {
+                    alert(String(no[i])+'你好')
+                    window.open("./load.html","點餐系統",config = 'height=800,width=600')
+                    return open_p
+                }
+                else
+                {
+                    alert("密碼錯誤!")
+                    break
+                }
             }
         }
-        
-
-        
-    }
+    });
+});
+function load(){
+    alert("目前沒功能")
+}   
     
-}
