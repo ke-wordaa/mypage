249160c8bb0ef522021b99ea7dcbd6d204c92055
kevinjj <h849179@gmail.com>
2022-06-23 23:41:13 +0800

    111_6_24

--- 表單/01.py
+++
@@ -1,4 +0,0 @@
-i=1
-while(i <= 47):
-    print(","+str(i))
-    i+=1
--- 表單/js/main.js
+++ 表單/js/main.js
@@ -1,52 +1,43 @@
 var no= [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47]
-let r =false
-
 $("body").ready(function () {
+    $('#push').attr('disabled', true);
     $('#push').css('background-color','gray');
-    Text_um = '帳號名稱(座號)'
-    $("#um").on('keyup', function () {
-        var t = $(this).val().length
-        if($(this).val() != ""){
-            $('#le').html('輸入字數:'+ `${t}`);
-            $('#push').css('background-color','#2691b9');
+    $("input").on('keyup', function () {
+        if($(this).val() != "")
+        {
+            if($('#pwd').val() === '163' && $('#um').val() != "" && $('#um').val()>=1 && $('#um').val()<=47){
+                    $('#push').attr('disabled', false);
+                    $('#push').css('background-color','#2691b9');
+                }
+            else{
+                    $('#push').attr('disabled', true);
+                    $('#push').css('background-color','gray');
+                }
         }
-        else{
-            console.log(Text_um)
-            $('#le').html(Text_um);
+        else
+        {
             $('#push').attr('disabled', true);
             $('#push').css('background-color','gray');
-
         }
     });
     $("#push").click(function () 
     { 
-        alert("a")
         var username = $("#um").val();
         var password = $("#pwd").val();
-        console.log(password)
-        
         for (let i = 0; i <= username; i++) {
             console.log(i)
             if (username == no[i]) {
                 if(password == "163")
                 {
-                    r=true
                     alert(String(no[i])+'你好')
-                    window.open("./load.html","點餐系統",config = 'height=800,width=600')
-                }
-                else
-                {
-                    alert("密碼錯誤!")
-                    break
+                    window.open("../page/load.html","點餐系統",config = 'height=800,width=600')
                 }
             }
         }
     });
 });
-
 function load(){
-    
     alert("目前沒功能")
     $("button").html(String (r));
-}   
+}       
     
