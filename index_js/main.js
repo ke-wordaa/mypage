043a4ea725ee64fa397ef308977fa876b09691de
kevimjj <h849179@ghmail.com>
2022-08-07 00:17:39 +0800

    8/7

---
+++ index_js/main.js
@@ -0,0 +1,14 @@
+$("body").ready(function () {
+    $('.b1').click(function () { 
+        alert(1)
+    });
+    $('.b2').click(function () { 
+        alert(2)
+    });
+    $('.b3').click(function () { 
+        alert(3)
+    });
+    $('.b4').click(function () { 
+        alert(4)
+    });
+});
