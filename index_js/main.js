6fc8b1db6663337cd3b933c323ba3bb1ee467c19
kevinjj <98082057+ke-wordaa@users.noreply.github.com>
2025-05-02 18:51:13 +0800

    114_05_02_1

--- index_js/main.js
+++ index_js/main.js
@@ -1,11 +1,11 @@
 $("body").ready(function () {
     $('.b0').click(function () {
         window.open('https://tcte.mykevinjj.dpdns.org/')
-    });us
+    });
     $('.b1').click(function () { 
        window.open('https://cap.mykevinjj.dpdns.org/')
     });
     $('.b2').click(function () { 
         window.open('https://gsat.mykevinjj.dpdns.org/')
-    });
+    }); 
 });
