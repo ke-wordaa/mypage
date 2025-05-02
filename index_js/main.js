005b4a238b03ff3b8013b78925050ff8e8d403c0
kevinjj <98082057+ke-wordaa@users.noreply.github.com>
2025-05-02 18:07:49 +0800

    114_05_02

--- index_js/main.js
+++ index_js/main.js
@@ -1,17 +1,11 @@
 $("body").ready(function () {
     $('.b0').click(function () {
-        window.open('https://www.mykevinjj.dpdns.org/')
+        window.open('https://tcte.mykevinjj.dpdns.org/')
     });us
     $('.b1').click(function () { 
-       window.open('./pj_1.html')
+       window.open('https://cap.mykevinjj.dpdns.org/')
     });
     $('.b2').click(function () { 
-        window.open('05/time.html')
-    });
-    $('.b3').click(function () { 
-        window.open('表單/index.html')
-    });
-    $('.b4').click(function () { 
-       window.open('4_21/4_21/index.html')
+        window.open('https://gsat.mykevinjj.dpdns.org/')
     });
 });
